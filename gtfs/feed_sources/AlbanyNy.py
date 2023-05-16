46ba9e4ecdfeb198aea389d0530e4e5011da7a78
Ananya Nayak <ananyanayak102@gmail.com>
2023-05-16 18:01:44 +0530

    feat: intersects bbox logic

--- gtfs/feed_sources/AlbanyNy.py
+++ gtfs/feed_sources/AlbanyNy.py
@@ -1,9 +1,7 @@
 """Fetch Capital District Transportation Authority (Albany, New York) feed."""
-from utils.FeedSource import FeedSource
+from ..feed_source import FeedSource
 
 class AlbanyNy(FeedSource):
     """Fetch CDTA feed."""
-    def __init__(self):
-        super(AlbanyNy, self).__init__()
-        self.url: str = 'http://www.cdta.org/schedules/google_transit.zip'
-        self.bbox: list[float] = [-74.219321, 42.467161, -73.614608, 43.10706]
+    url: str = 'http://www.cdta.org/schedules/google_transit.zip'
+    bbox: list[float] = [-74.219321, 42.467161, -73.614608, 43.10706]
