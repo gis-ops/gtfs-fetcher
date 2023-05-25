4561720af91d07a4e9b378ace6f22dccbba68251
Ananya Nayak <ananyanayak102@gmail.com>
2023-05-25 12:02:33 +0530

    moved geom tests to a separate util dir

--- gtfs/utils/constants.py
+++ gtfs/utils/constants.py
@@ -1,26 +1,6 @@
 from enum import Enum
 
-from gtfs.utils.geom import Bbox
-
 
 class Predicate(str, Enum):
     intersects = "intersects"
     contains = "contains"
-
-
-# example inputs for testing geom funcs
-
-
-bbox_covers_bbox = (Bbox(0, 0, 10, 10), Bbox(0, 0, 10, 10))
-bbox_contained_inside_bbox = (Bbox(1, 1, 5, 5), Bbox(0, 0, 10, 10))
-bbox_outside_bbox = (Bbox(15, 15, 20, 20), Bbox(0, 0, 10, 10))
-bbox_intersecting_bbox = (Bbox(1, 1, 5, 5), Bbox(2, 2, 10, 15))
-bbox_touches_edge_only_bbox = (Bbox(5, 1, 10, 5), Bbox(0, 0, 5, 15))
-
-__all__ = [
-    "bbox_covers_bbox",
-    "bbox_contained_inside_bbox",
-    "bbox_outside_bbox",
-    "bbox_intersecting_bbox",
-    "bbox_touches_edge_only_bbox",
-]
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -2,48 +2,11 @@ import pytest
 from typer.testing import CliRunner
 
 from gtfs.__main__ import app
-from gtfs.utils.constants import *
-from gtfs.utils.geom import bbox_contains_bbox, bbox_intersects_bbox
 
 
 @pytest.fixture(scope="module")
 def runner():
     return CliRunner()
-
-
-class Test_Geom_Functions:
-    @pytest.mark.parametrize(
-        "bbox1, bbox2",
-        [bbox_covers_bbox, bbox_contained_inside_bbox],
-    )
-    def test_geom_contains(self, bbox1, bbox2):
-        assert bbox_contains_bbox(bbox1, bbox2) == True
-
-    @pytest.mark.parametrize(
-        "bbox1, bbox2",
-        [bbox_outside_bbox, bbox_intersecting_bbox],
-    )
-    def test_geom_contains_not(self, bbox1, bbox2):
-        assert bbox_contains_bbox(bbox1, bbox2) == False
-
-    @pytest.mark.parametrize(
-        "bbox1, bbox2",
-        [
-            bbox_covers_bbox,
-            bbox_contained_inside_bbox,
-            bbox_intersecting_bbox,
-            bbox_touches_edge_only_bbox,
-        ],
-    )
-    def test_geom_intersects(self, bbox1, bbox2):
-        assert bbox_intersects_bbox(bbox1, bbox2) or bbox_intersects_bbox(bbox2, bbox1) == True
-
-    @pytest.mark.parametrize(
-        "bbox1, bbox2",
-        [bbox_outside_bbox],
-    )
-    def test_geom_intersects_not(self, bbox1, bbox2):
-        assert bbox_intersects_bbox(bbox1, bbox2) or bbox_intersects_bbox(bbox2, bbox1) == False
 
 
 class Test_List_Feeds_Command:
