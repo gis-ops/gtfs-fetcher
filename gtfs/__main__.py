00220165ecba4be3eb0c889caa3adcedab489d09
Ananya Nayak <ananyanayak102@gmail.com>
2023-06-08 14:08:55 +0530

    chore: improve list-feeds command (#4)
    * chore: improved list-feeds command
    
    * some cleanup

--- gtfs/__main__.py
+++ gtfs/__main__.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python
 """Command line interface for fetching GTFS."""
 import logging
+from typing import Optional
 
 import typer
 from prettytable.colortable import ColorTable, Themes
 from typing_extensions import Annotated
 
 from .feed_source import FeedSource
-from .feed_sources import __all__ as feed_sources
-from .utils.constants import Predicate, console, spinner, success
+from .feed_sources import feed_sources
+from .utils.constants import Predicate, spinner
 from .utils.geom import Bbox, bbox_contains_bbox, bbox_intersects_bbox
 
 logging.basicConfig()
@@ -16,7 +17,9 @@ LOG = logging.getLogger()
 app = typer.Typer()
 
 
-def check_bbox(bbox: str) -> Bbox:
+def check_bbox(bbox: str) -> Optional[Bbox]:
+    if bbox is None:
+        return
     try:
         min_x, min_y, max_x, max_y = [float(coord) for coord in bbox.split(",")]
     except ValueError as e:
@@ -39,50 +42,72 @@ def check_bbox(bbox: str) -> Bbox:
 @app.command()
 def list_feeds(
     bbox: Annotated[
-        str,
+        Optional[str],
         typer.Option(
             "--bbox",
             "-b",
             help="pass value as a string separated by commas like this: min_x,min_y,max_x,max_y",
             callback=check_bbox,
         ),
-    ],
+    ] = None,
     predicate: Annotated[
-        Predicate,
+        Optional[Predicate],
         typer.Option(
             "--predicate",
-            "-p",
+            "-pd",
             help="the gtfs feed should intersect or should be contained inside the user's bbox",
         ),
-    ] = Predicate.intersects,
+    ] = None,
+    pretty: Annotated[
+        bool,
+        typer.Option(
+            "--pretty",
+            "-pt",
+            help="display feeds inside a pretty table",
+        ),
+    ] = False,
 ) -> None:
     """Filter feeds spatially based on bounding box."""
-    spinner("Fetching feeds...", 1)
-    ptable = ColorTable(["Feed Source", "Transit URL", "Bounding Box"], theme=Themes.OCEAN)
-    for src in feed_sources:
-        feed_bbox: Bbox = src.bbox
-        if predicate == "contains":
-            if not bbox_contains_bbox(feed_bbox, bbox):
-                continue
-        elif predicate == "intersects":
-            if (not bbox_intersects_bbox(feed_bbox, bbox)) and (
-                not bbox_intersects_bbox(bbox, feed_bbox)
-            ):
+    if bbox is None and predicate is not None:
+        raise typer.BadParameter(
+            f"Please pass a bbox if you want to filter feeds spatially based on predicate = {predicate}!"
+        )
+    elif bbox is not None and predicate is None:
+        raise typer.BadParameter(
+            f"Please pass a predicate if you want to filter feeds spatially based on bbox = {bbox}!"
+        )
+    else:
+        spinner("Fetching feeds...", 1)
+        if pretty is True:
+            pretty_output = ColorTable(
+                ["Feed Source", "Transit URL", "Bounding Box"], theme=Themes.OCEAN, hrules=1
+            )
+
+        for src in feed_sources:
+            feed_bbox: Bbox = src.bbox
+            if bbox is not None and predicate == "contains":
+                if not bbox_contains_bbox(feed_bbox, bbox):
+                    continue
+            elif bbox is not None and predicate == "intersects":
+                if (not bbox_intersects_bbox(feed_bbox, bbox)) and (
+                    not bbox_intersects_bbox(bbox, feed_bbox)
+                ):
+                    continue
+
+            if pretty is True:
+                pretty_output.add_row(
+                    [
+                        src.__name__,
+                        src.url,
+                        [feed_bbox.min_x, feed_bbox.min_y, feed_bbox.max_x, feed_bbox.max_y],
+                    ]
+                )
                 continue
 
-        row = [
-            src.__name__,
-            src.url,
-            [feed_bbox.min_x, feed_bbox.min_y, feed_bbox.max_x, feed_bbox.max_y],
-        ]
-        ptable.add_row(row)
+            print(src.url)
 
-    print(
-        "\n" + f"Feeds based on bbox input {bbox} and "
-        f"for predicate={predicate.value} are as follows:"
-    )
-    print("\n" + ptable.get_string())
-    console.print("All done!", style=success)
+        if pretty is True:
+            print("\n" + pretty_output.get_string())
 
 
 @app.command()
@@ -92,26 +117,22 @@ def fetch_feeds(sources=None):
     """
     statuses = {}  # collect the statuses for all the files
 
-    # make a copy of the list of all modules in feed_sources;
     # default to use all of them
     if not sources:
-        sources = list(feed_sources.__all__)
+        sources = feed_sources
 
     LOG.info("Going to fetch feeds from sources: %s", sources)
     for src in sources:
         LOG.debug("Going to start fetch for %s...", src)
         try:
-            module = getattr(feed_sources, src)
-            # expect a class with the same name as the module; instantiate and fetch its feeds
-            klass = getattr(module, src)
-            if issubclass(klass, FeedSource):
-                inst = klass()
+            if issubclass(src, FeedSource):
+                inst = src()
                 inst.fetch()
                 statuses.update(inst.status)
             else:
-                LOG.warn(
+                LOG.warning(
                     "Skipping class %s, which does not subclass FeedSource.",
-                    klass.__name__,
+                    src.__name__,
                 )
         except AttributeError:
             LOG.error("Skipping feed %s, which could not be found.", src)
@@ -120,8 +141,18 @@ def fetch_feeds(sources=None):
     if "last_check" in statuses:
         del statuses["last_check"]
 
-    # display results
-    ptable = ColorTable()
+    ptable = ColorTable(
+        [
+            "file",
+            "new?",
+            "valid?",
+            "current?",
+            "newly effective?",
+            "error",
+        ],
+        theme=Themes.OCEAN,
+        hrules=1,
+    )
 
     for file_name in statuses:
         stat = statuses[file_name]
@@ -137,14 +168,6 @@ def fetch_feeds(sources=None):
             msg.append("")
         ptable.add_row(msg)
 
-    ptable.field_names = [
-        "file",
-        "new?",
-        "valid?",
-        "current?",
-        "newly effective?",
-        "error",
-    ]
     LOG.info("Results:\n%s", ptable.get_string())
     LOG.info("All done!")
 
